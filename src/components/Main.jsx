89003a98a53e71d1aa29a9e70d79dabbfa97d940
miriyan19425 <miriyan19425@gmail.com>
2025-05-31 00:59:12 +0300

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Form from "./Form";
+import DataView from "./DataView";
+
+function Main(){
+    return (
+    <div id="content">
+        <Form />
+        <DataView />
+    </div>  
+    );
+}
+
+export default Main;

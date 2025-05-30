0989cdcf7d9e571040eed6004829ab49da232fdf
miriyan19425 <miriyan19425@gmail.com>
2025-05-31 00:54:54 +0300

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Main from "./Main";
+
+function Body(){
+    return (
+        <div className="body">
+            <h1>Active Minutes</h1>
+            <h4>Track Your Daily Activity</h4>
+            <Main />
+        </div>
+    );
+}
+
+export default Body;

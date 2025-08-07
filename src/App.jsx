efad246245a0c16daf929e4ad67c47941a61f395
miriyan19425 <miriyan19425@gmail.com>
2025-08-07 23:02:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ActivityForm from "./components/ActivityForm";
+import ActivityPreview from "./components/ActivityPreview";
+import ActivitiesTable from "./components/ActivitiesTable";
 
 function App() {
    return (
@@ -7,25 +9,9 @@ function App() {
   <h4>Track Your Daily Activity</h4>
   <main id="content">
       <ActivityForm />
-
     <div className="data-view">
-      <div className="preview">
-        <ul id="preview-activity"></ul>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Activity Type</th>
-            <th>Duration (mins)</th>
-            <th>Calories Burned</th>
-            <th>Date</th>
-            <th>Intensity</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody id="activities-table">
-        </tbody>
-      </table>
+      <ActivityPreview />
+      <ActivitiesTable />
     </div>
   </main>
    

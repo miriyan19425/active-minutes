c48f23ada2843c82a40f9e0c1e4efe6542d30679
miriyan19425 <miriyan19425@gmail.com>
2025-08-09 17:10:13 +0300

    Update ActivitiesTable.jsx

--- src/components/ActivitiesTable.jsx
+++ src/components/ActivitiesTable.jsx
@@ -1,19 +1,19 @@
 function ActivitiesTable(){
     return (
         <table>
-        <thead>
-          <tr>
-            <th>Activity Type</th>
-            <th>Duration (mins)</th>
-            <th>Calories Burned</th>
-            <th>Date</th>
-            <th>Intensity</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody id="activities-table">
-        </tbody>
-      </table>
+            <thead>
+                <tr>
+                    <th>Activity Type</th>
+                    <th>Duration (mins)</th>
+                    <th>Calories Burned</th>
+                    <th>Date</th>
+                    <th>Intensity</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody id="activities-table">
+            </tbody>
+        </table>
     );
 }
 

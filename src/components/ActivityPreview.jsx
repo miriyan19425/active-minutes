d14599f606aa0424fc86fed13ba8cd2a2c849ad7
miriyan19425 <miriyan19425@gmail.com>
2025-08-19 00:38:53 +0300

    Update ActivityPreview.jsx

--- src/components/ActivityPreview.jsx
+++ src/components/ActivityPreview.jsx
@@ -3,7 +3,8 @@ function ActivityPreview({activityInput}){
         <div className="preview">
             <ul id="preview-activity">
             {
-            (activityInput.type === 'Choose Type'
+            (activityInput.type === 'Choose Type' &&
+             activityInput.intensity === 'Choose Intensity' &&
             ) ||
             <li>
             

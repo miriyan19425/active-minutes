11d63cff7cb029c969bf6cccab60f318ad75cb02
miriyan19425 <miriyan19425@gmail.com>
2025-08-20 00:08:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ActivitiesTable from "./components/ActivitiesTable";
 
 function App() {
    const [activityData, setActivityData] = useState({type: 'Choose Type', intensity: 'Choose Intensity', calories: 0, duration: 0, date: ''});
+
+   function getActivityData(d) { setActivityData(d); }
    
    return (
         <div className="body">

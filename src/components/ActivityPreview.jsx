911192909ac450a0a5d64cd71dfc92bcab82bc52
miriyan19425 <miriyan19425@gmail.com>
2025-08-21 18:13:06 +0300

    Update ActivityPreview.jsx

--- src/components/ActivityPreview.jsx
+++ src/components/ActivityPreview.jsx
@@ -1,19 +1,27 @@
 function ActivityPreview({activityInput}){
-    return (
-        <div className="preview">
-            <ul id="preview-activity">
-            {
-            (activityInput.type === 'Choose Type' &&
-             activityInput.intensity === 'Choose Intensity' &&
-             activityInput.calories === 0
-            ) ||
-            <li>
-            
-            </li>
-            }
-            </ul>
-        </div>
-    );
+  return (
+    <div className="preview">
+      {(  
+        activityInput.type === 'Choose Type' && 
+        activityInput.intensity === 'Choose Intensity' &&
+         
+      ) ||
+        <ul id="preview-activity">
+          <li>
+            <article>
+              <p>Type: {activityInput.type}</p>
+              <p>Intensity: {activityInput.intensity}</p>
+               
+            </article>
+            <div className="btn-container">
+              <button className="edit-btn">Edit</button>
+              <button className="next-btn">Next</button>
+            </div>
+          </li>
+        </ul>
+      }  
+    </div>
+  );
 }
 
 export default ActivityPreview;
